d57da4a77afb54a7393f9b78f532a7492680505d
Yoyofeng <2216179464@qq.com>
2017-04-21 12:16:40 +0800

    加入其它页面后的配置

---
+++ modules/cn/controllers/IeltssheetController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/21 0021
+ * Time: 10:40
+ */
+namespace app\modules\cn\controllers;
+use yii;
+use yii\web\controller;
+class IeltssheetController extends Controller{
+    public function actionIndex(){
+        return $this->renderPartial('index');
+    }
+}
