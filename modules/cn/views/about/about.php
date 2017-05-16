7f5210922b16c851dff89881c09999a77353c21b
daicunya <daicunya@163.com>
2017-05-16 15:19:42 +0800

    公共登录注册

--- modules/cn/views/about/about.php
+++ modules/cn/views/about/about.php
@@ -217,72 +217,7 @@
   <?php use app\commands\front\FootWidget;?>
   <?php FootWidget::begin();?>
   <?php FootWidget::end();?>
-  <!--登录、注册框-->
-  <div class="s-login">
-      <div class="s-login-cnt">
-          <h2>会员登录</h2>
-          <form action="" onsubmit="return check()">
-              <input type="text" class="form-control" id="loginName" onblur="checkUser('loginName')" placeholder="请输入手机号/邮箱">
-              <input type="password" class="form-control" id="loginPass" placeholder="请输入密码">
-              <button type="submit" class="btn btn-info s-btn" onclick="login()" id="loginBtn">登录</button>
-              <div>
-                  <label>记住密码</label>
-                  <input class="s-rember-pwd" type="checkbox" checked="checked">
-                  <a href="#" class="pull-right">忘记密码?</a>
-              </div>
-          </form>
-          <div class="s-login-bottom">
-              <a class="s-go-sign" href="#">免费注册</a>
-              <div>
-                  <span>第三方登录:</span>
-                  <a href="#">
-                      <img src="/cn/images/weChat.png" alt="微信图标">
-                  </a>
-                  <a href="#">
-                      <img src="/cn/images/QQ.png" alt="qq图标">
-                  </a>
-              </div>
-          </div>
-          <i class="icon-remove"></i>
-      </div>
-      <!--注册-->
-      <div class="s-sign-cnt">
-          <ul class="nav nav-tabs" role="tablist">
-              <li role="presentation" class="active"><a href="#sPhone" aria-controls="sPhone" role="tab" data-toggle="tab">手机注册</a></li>
-              <li role="presentation"><a href="#sEmail" aria-controls="sEmail" role="tab" data-toggle="tab">邮箱注册</a></li>
-          </ul>
-          <div class="tab-content">
-              <div role="tabpanel" class="tab-pane active" id="sPhone">
-                  <input type="email" class="form-control" id="signTel" onblur="signTel(this.value)" placeholder="请输入手机号">
-                  <input type="password" class="form-control" id="signPwd1" onblur="signPwd1(this.value)" placeholder="请输入密码">
-                  <input type="text" class="form-control s-sign-code" id="signCode" placeholder="请输入验证码"><button>点击获取验证码</button>
-              </div>
-              <div role="tabpanel" class="tab-pane" id="sEmail">
-                  <input type="email" class="form-control" id="signEmail" onblur="signEmail(this.value)" placeholder="请输入邮箱">
-                  <input type="password" class="form-control" id="signPwd2" onblur="signPwd2(this.value)" placeholder="请输入密码">
-              </div>
-          </div>
-          <form action="" onsubmit="return check()">
-              <button type="submit" class="btn btn-info s-btn s-register">注册</button>
-          </form>
-          <from class="s-sign-agree">
-              <input type="checkbox"><span>我已阅读并同意<a href="#">申友协议</a></span>
-          </from>
-          <div class="s-sign-bottom">
-              <a class="s-login-back" href="#">返回登录</a>
-              <div>
-                  <span>第三方登录:</span>
-                  <a href="#">
-                      <img src="/cn/images/weChat.png" alt="微信图标">
-                  </a>
-                  <a href="#">
-                      <img src="/cn/images/QQ.png" alt="qq图标">
-                  </a>
-              </div>
-              <i class="icon-remove"></i>
-          </div>
-      </div>
-  </div>
+
 </body>
 <script>
   $(function () {
