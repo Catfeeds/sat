8ffb65674388c10daf3d07d9be1e858ac3ea4617
daicunya <daicunya@163.com>
2017-05-26 10:20:00 +0800

    分页

--- modules/cn/controllers/PubClassController.php
+++ modules/cn/controllers/PubClassController.php
@@ -44,15 +44,15 @@ class PubclassController extends Controller
     public function actionPage()
     {
         $p = Yii::$app->request->get('p','1');
-        $pagesize=6;
+        $pagesize=2;
         $data= Yii::$app->db->createCommand("select * from {{%info}} where isShow=0 and cate='公开课' limit ".($p-1)*$pagesize.",".$pagesize)->queryAll();
         $re= Yii::$app->db->createCommand("select count(id) from {{%info}} where isShow=0 and cate='公开课'")->queryAll();
-//        var_dump($re);die;
         $total = $re[0]['count(id)'];//总记录数
         $totalPage = ceil($total/$pagesize);// 总页数
         $arr['total'] = $total;
         $arr['pageSize'] = $pagesize;
         $arr['totalPage'] = $totalPage;
+        $arr['curPage'] = $p;
         foreach($data as $k=>$v){
             $arr['list'][]= array(
                 'summary' => $v['summary'],
