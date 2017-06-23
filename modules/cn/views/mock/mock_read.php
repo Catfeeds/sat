f07dc241bca7bef3ad1072db7ab24fd18d79660c
daicunya <daicunya@163.com>
2017-06-23 17:18:10 +0800

    添加模考测评详情页

---
+++ modules/cn/views/mock/mock_read.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>模考-测评详情页</title>
+    <link rel="stylesheet" href="/cn/css/reset.css">
+    <link rel="stylesheet" href="/cn/css/bootstrap.css">
+    <link rel="stylesheet" href="/cn/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/cn/css/mock-details.css">
+
+    <script src="/cn/js/jquery-2.1.3.js"></script>
+    <script src="/cn/js/bootstrap.js"></script>
+    <script src="/cn/js/jquery.SuperSlide.2.1.js"></script>
+    <script src="/cn/js/mock-details.js"></script>
+</head>
+<body>
+<div class="work-mk">
+    <div class="work-mk-top container">
+        <div class="work-top-cnt row">
+            <p class="work-title-pos col-lg-3 col-md-3">第(1/5)题</p>
+            <h1 class="work-main-title col-lg-6 col-md-6">Master the new SAT 6<i>--</i><span class="work-subhead">SAT Reading Section1</span></h1>
+            <p class="work-collect col-lg-3 col-md-3"><i class="icon-star-empty">&nbsp;</i>收藏</p>
+        </div>
+    </div>
+    <div class="work-mk-cnt clearfix">
+        <div class="work-wrap-left pull-left">
+            <h3>QUESTIONS 12-22 ARE BASED ON THE FOLLOWING PASSAGE.</h3>
+            <h5>The following is an excerpt from a speech given by President Jimmy Carter: spoken and broadcast from the White House library two weeks after he took office. Prior to Carter's election, the country had faced a severe oil shortage and rising prices for oil and related products.</h5>
+            <div class="work-box">
+                <div class="read-text">
+                    <p>
+                        "The Glass Ceiling" is a metaphor that refers to the imperceptible and subversive forms of discrimination women in the workforce encounter when pursuing upper-level management positions. A popular phrase in the 1980s, it has become less widely used over the ensuing years. [Q1] 0ther phrases from past decades are still in frequent use. However, as recent data makes clear, "The Glass Ceiling" remains a solid barrier to women working in [Q2] America!
+                        According to data from the U.S. Bureau of Labor Statistics and the Pew Research Center, women make up 57.2% of the nation's workforce [Q3] but hold 5 percent only of CEO positions in Fortune 500 companies. Women also account for only 17% of board membership at Fortune 500 companies. While these percentages note a minimal increase in female leadership over the [Q4] passed thirty years, barriers still seem to exist.
+                        [Q5] In 1995, a bipartisan Federal Glass Ceiling Commission was formed; to determine the root of gender discrimination within corporate America. The committee, headed by Secretary of Labor Robert Reich, noted that "At the highest levels of corporations the promise of reward for preparation and pursuit of excellence is not equally available to members of all groups." Two barriers that contributed most to "The Glass Ceiling" [Q6] effect were "supply barriers" and "internal business barriers."
+                        "Supply barriers" represent the lack of leadership, education, and experience women are given access to during high school and college. Corporations have viewed this lack of access as an educational reform issue, not a business issue, and [Q7] they do not think it's their responsibility to fix it. However, corporations are equipped with both financial and educational resources to overcome this "supply barrier." If corporations are serious about ending discrimination in the workplace, they need to invest in educational programs that train women to be future leaders. This includes creating more mentoring programs and school-to-work initiatives and providing more educational scholarships. [Q8]
+                        [Q9] The difference between what corporate executives say they want to do. In regard to discrimination, and efforts being made (or not made) to end discriminatory practices is another issue. Talking the talk and not walking the walk is what the "internal business barrier" is all about. The reason for this discrepancy is that many white males working in the highly competitive corporate America believe they are "losing the corporate game, losing control, and losing opportunity." In essence, white men in corporate leadership feel threatened by including women in their ranks.
+                        [Q10] Ending discrimination against women in the workforce is an involved, complicated process. While many efforts have been made to prevent overt discriminatory practices, 65% of women still see gender bias as a barrier they have to overcome. [Q11] Until women can be as equals accepted in the corporate sphere, "The Glass Ceiling" will remain firmly intact.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="work-wrap-right pull-right">
+            <div class="work-question" id="1">
+                <div class="work-question-part clearfix">
+                    <h3>1.Over the course of the passage, the narrator’s attitude shifts from</h3>
+                    <ul class="work-que-list" data-id="2358636904440836">
+                        <li class="work-que-wrap clearfix">
+                            <div class="work-select" data-id="A">A</div>
+                            <div class="work-que">describe a boy's reactions to his irresponsible parents. </div>
+                        </li>
+                        <li class="work-que-wrap clearfix">
+                            <div class="work-select" data-id="B">B</div>
+                            <div class="work-que">describe a boy's reactions to his irresponsible parents. describe a boy's reactions to his irresponsible parents.</div>
+                        </li>
+                        <li class="work-que-wrap clearfix">
+                            <div class="work-select" data-id="C">C</div>
+                            <div class="work-que">describe a boy's reactions to his irresponsible parents. </div>
+                        </li>
+                        <li class="work-que-wrap clearfix">
+                            <div class="work-select" data-id="D">D</div>
+                            <div class="work-que">describe a boy's reactions to his irresponsible parents. </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--数学-->
+    <!--<div class="work-mk-cnt work-mk-math">-->
+    <!--<div class="work-question-part">-->
+    <!--<h3>1.Over the course of the passage, the narrator’s attitude shifts from</h3>-->
+    <!--<ul class="work-que-list" data-id="2358636904440836">-->
+    <!--<li class="work-que-wrap clearfix">-->
+    <!--<div class="work-select" data-id="A">A</div>-->
+    <!--<div class="work-que">describe a boy's reactions to his irresponsible parents. </div>-->
+    <!--</li>-->
+    <!--<li class="work-que-wrap clearfix">-->
+    <!--<div class="work-select" data-id="B">B</div>-->
+    <!--<div class="work-que">describe a boy's reactions to his irresponsible parents. describe a boy's reactions to his irresponsible parents.</div>-->
+    <!--</li>-->
+    <!--<li class="work-que-wrap clearfix">-->
+    <!--<div class="work-select" data-id="C">C</div>-->
+    <!--<div class="work-que">describe a boy's reactions to his irresponsible parents. </div>-->
+    <!--</li>-->
+    <!--<li class="work-que-wrap clearfix">-->
+    <!--<div class="work-select" data-id="D">D</div>-->
+    <!--<div class="work-que">describe a boy's reactions to his irresponsible parents. </div>-->
+    <!--</li>-->
+    <!--</ul>-->
+    <!--</div>-->
+    <!--</div>-->
+
+    <!--数学填空-->
+    <!--<div class="work-mk-cnt work-math-gap">
+      <div class="work-question-part">
+        <h3>1.Over the course of the passage, the narrator’s attitude shifts from</h3>
+        <table class="math-gap-table" border="1" align="center">
+          <tr>
+            <td class="math-gap-result" colspan="4"><input type="text"></td>
+          </tr>
+          <tr>
+            <td class="math-btn">7</td>
+            <td class="math-btn">8</td>
+            <td class="math-btn">9</td>
+            <td class="math-sure" rowspan="2">确定</td>
+          </tr>
+          <tr>
+            <td class="math-btn">4</td>
+            <td class="math-btn">5</td>
+            <td class="math-btn">6</td>
+          </tr>
+          <tr>
+            <td class="math-btn">1</td>
+            <td class="math-btn">2</td>
+            <td class="math-btn">3</td>
+            <td class="math-clear" rowspan="2">清空</td>
+          </tr>
+          <tr>
+            <td class="math-btn">0</td>
+            <td class="math-btn">.</td>
+            <td class="math-btn">/</td>
+          </tr>
+        </table>
+      </div>
+    </div>-->
+
+    <div class="work-mk-btm container">
+        <div class="work-btm-cnt row">
+            <div class="work-out col-lg-1 col-md-1">
+                <i class="work-out-off icon-off"></i>
+                <span>离开</span>
+            </div>
+            <div class="work-time col-lg-3 col-md-3">
+                <i class="work-time-icon icon-time"></i>
+                <span class="work-time-cnt"></span>
+            </div>
+            <div class="work-sec-process col-lg-4 col-md-4">
+                本section进度:<span>3</span>/<span>55</span>
+            </div>
+            <div class="work-all-process col-lg-3 col-md-3">
+                做题总进度:<span>30</span>/<span>155</span>
+            </div>
+            <div class="work-btm-next col-lg-1 col-md-1">
+                <a href="#">
+                    <i class="work-next-icon icon-caret-right"></i>NEXT</a>
+            </div>
+        </div>
+    </div>
+    <!--遮罩层-->
+    <div class="work-shade">
+        <div class="quit-wrap shade-wrap">
+            <h3>小主,你忍心弃我而去吗?</h3>
+            <div class="shade-select clearfix">
+                <span class="exit-out shade-out pull-left">忍心而去</span>
+                <span class="shade-in pull-right">逗你玩呢!</span>
+            </div>
+        </div>
+        <div class="next-wrap shade-wrap">
+            <h3>答案都木有</h3>
+            <h4>(根据SAT考试规定,按照答对题目数得分)</h4>
+            <div class="shade-select clearfix">
+                <span class="shade-out pull-left">我就是不做</span>
+                <span class="shade-in pull-right">这么简单,我来答</span>
+            </div>
+        </div>
+        <div class="auto-wrap shade-wrap">
+            <h3>答题时间到,点击确定进入到下一小节</h3>
+            <div class="shade-select">
+                <span class="make-sure shade-in">确定</span>
+            </div>
+        </div>
+    </div>
+    <!--隐藏数据-->
+    <div class="worl-btm-hidden">
+        <!--本section总时间-->
+        <input type="hidden" id="sectionTime" value="1">
+        <!--本section总题目-->
+        <input type="hidden" id="sectionNum" value="67">
+    </div>
+</div>
+</body>
+</html>
