6f54ac85843d89793eafc2320dcc087ef8387a9d
daicunya <daicunya@163.com>
2017-06-27 15:21:37 +0800

    公开课显示数据修改

--- modules/cn/views/sat/index.php
+++ modules/cn/views/sat/index.php
@@ -58,9 +58,11 @@
                     <?php foreach($info1 as $v){?>
                     <li class="s-cnt">
                         <img src="<?php echo $v['pic']?>" alt="">
-                        <p>主题：名师带你走进高分殿堂</p>
-                        <p>时间：2017-8-5</p>
-                        <p>嘉宾：Amanda</p>
+                        <div class="s-pubclass-info">
+                            <p>主题：名师带你走进高分殿堂</p>
+                            <p>时间：2017-8-5</p>
+                            <p>嘉宾：Amanda</p>
+                        </div>
                         <a href="/info_details/<?php echo $v['id']?>.html">查看详情</a>
                     </li>
                     <?php }?>
