b21fcd5ffeafeac112fde98c3478fdf1cdb30ae5
daicunya <daicunya@163.com>
2017-07-12 17:47:08 +0800

    mk

--- web/cn/js/public.js
+++ web/cn/js/public.js
@@ -10,20 +10,11 @@ $(function () {
     }
   })
   //APP下载
-  $('.appDownload').on({
-    mouseenter: function () {
+  $('.appDownload').mouseenter(function () {
     var _this=  $(this);
     _this.css({'backgroundColor':'#f3f7f7'});
     _this.children('span').css('color','#000');
     $('.app-down').show();
-  },
-    mouseleave: function() {
-    var _this = $(this);
-    $('.app-down').hide();
-    $('.app-box').hide();
-    _this.css({'backgroundColor':'#3d3d3d'});
-    _this.children('span').css('color','#f0f0f0');
-  }
   })
   $('.app-down li').mouseenter(function () {
     var _this = $(this);
@@ -32,33 +23,12 @@ $(function () {
     _this.addClass('on');
     _this.children('.app-box').show();
   })
-//  模考收藏点击事件
-  $('.work-collect').click(function () {
-    collectEvent(this);
-  });
-//  练习、模考选择题点击事件
-  $('.work-que-wrap').click(function () {
-    $('.work-select').removeClass('active');
-    $(this).find('.work-select').addClass('active');
-  })
-//  练习、模考数学填空点击事件
-  var result = $('.math-gap-result input');
-  $('.math-btn').click(function () {
-    result.get(0).value += $(this).html();
-  })
-  $('.math-clear').click(function () {
-    result.val('');
-  })
-  $('.math-sure').click(function () {
-    $('.math-gap-table tr').addClass('sure');
-    $('.math-gap-table').addClass('sure');
-    $('.math-gap-table td').hover(function () {
-      $('.math-gap-table td').css({
-        'color': '#ccc',
-        'backgroundColor': '#f1f1f1'
-      })
-    });
-    $('.math-btn').removeClass('math-btn');
+  $('.appDownload').mouseleave(function () {
+    var _this = $(this);
+    $('.app-down').hide();
+    $('.app-box').hide();
+    _this.css({'backgroundColor':'#3d3d3d'});
+    _this.children('span').css('color','#f0f0f0');
   })
   //  侧边栏
   $('.side-bar li').mousemove(function () {
@@ -68,11 +38,11 @@ $(function () {
   $('.side-bar li').mouseout(function () {
     $(this).children('div').hide();
   })
-  //小火箭
+  //小火箭置顶
   $('.side-arrow').click(function () {
    $('html,body').animate({scrollTop:0},'slow');
   })
-  //  底部课程报名
+  //SAT课程报名提交用户信息
   $('.apply-submit').click(function () {
     var name = $('.apply-name').val(),
         country = $('.apply-country').val(),
@@ -80,7 +50,7 @@ $(function () {
         time = $('.apply-time').val(),
         email = $('.apply-email').val(),
         tel = $('.apply-tel').val(),
-        code = $('.apply-code').val();
+        code = $('.apply-cde').val();
     if (name == '') {
       alert('请填写姓名哦！')
     } else if(time == '') {
@@ -91,67 +61,48 @@ $(function () {
       alert('请输入手机号')
     } else if(code == '') {
       alert('验证码怎么能为空呢！')
+    }else {
+      $.ajax({
+        type: 'GET',
+        url: '/cn/message/index',
+        data: {
+          'name':name,
+          'country':country,
+          'score':score,
+          'time':time,
+          'email':email,
+          'tel':tel,
+          'code':code
+        },
+        dataType: 'json',
+        success: function (data) {
+          alert(data.message);
+        },
+        error: function(XMLHttpRequest){
+          alert( "Error:" + XMLHttpRequest.responseText);
+        }
+      })
     }
-    //$.post('',{
-    //  'name':name,
-    //  'country':country,
-    //  'score':score,
-    //  'time':time,
-    //  'email':email,
-    //  'tel':tel,
-    //  'code':code
-    //}, function (data) {
-    //  alert(data);
-    //})
   })
   $('.apply-close').click(function () {
     $('.apply-fix').hide();
   })
 })
-//获取uid
-var uId = $.cookie('uid');
-//var uId = 444;
-//收藏函数
-function collectEvent(obj) {
-  if (uId == '') {
-    alert('登陆后才可以收藏哦！')
-  }else {
-    var _this = $(obj),
-        subjectId = $('#subjectId').data('id'),
-        val = $('.work-collect').data('value');
-    $.ajax({
-      type: 'get',
-      url: '/cn/collection/collection',
-      data: {
-        uid: uId,
-        subID: subjectId,
-        val:  val
-      },
-      dataType: 'json',
-      success: function(data) {
-        if (data.code == 1) {//收藏成功
-          _this.addClass('active');
-          _this.find('i').removeClass('fa-star-o');
-          _this.find('i').addClass('fa-star');
-          _this.data('value',1);
-          alert(data.message);
-        } else if(data.code == 2){//取消收藏
-          _this.removeClass('active');
-          _this.find('i').removeClass('fa-star');
-          _this.find('i').addClass('fa-star-o');
-          _this.data('value',0);
-          alert(data.message);
-        } else {
-          alert(data.message);
-        }
-      },
-      error: function (data) {
-        alert('操作失败！');
-      }
-    })
+//  邮箱验证
+function ckEmail(obj) {
+  var objValue = $(obj).val(),
+      rules = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+\.){1,63}[a-z0-9]+$/;
+  if (objValue.match(rules) == null) {
+    alert('请输入正确的邮箱！')
   }
 }
-
+// 发送手机验证码
+function leftCode(){
+  var tel = $('.apply-tel').val();
+  $.post('/user/api/phone-code',{type:2,phoneNum:tel},function(re){
+    alert(re.message);
+  },"json")
+}
 //禁止右键
 // function stop(){
 //   return false;
