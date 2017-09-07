7f57c289be89636d4ddae2854de053f228e76ad4
daicunya <daicunya@163.com>
2017-09-07 14:20:47 +0800

    测评报告页面

---
+++ modules/cn/views/evaulation/report.php
@@ -0,0 +1,246 @@
+<link rel="stylesheet" href="/cn/css/eval-report.css">
+<section class="s-w1200">
+<!--  广告图-->
+  <div class="eval-bnr">
+    <div><a href="#">历史测评报告&nbsp;<i>&gt;</i></a></div>
+  </div>
+<!--测评报告成绩-->
+  <div class="score">
+    <div class="title">
+      <h1>成绩分析</h1>
+    </div>
+    <!--内容-->
+    <div class="wrap">
+      <ul class="score-top clearfix">
+        <li>总正确率<span>60</span>%</li>
+        <li>你击败了<span>5</span>人</li>
+      </ul>
+      <div class="score-cnt clearfix">
+        <div class="score-img fl">
+          <img src="/cn/images/eval-report02.png" alt="图片">
+        </div>
+        <div class="score-table fl">
+          <table>
+            <tr>
+              <th>分数</th>
+              <th>语言分析能力</th>
+              <th>词汇量预估</th>
+              <th>理解能力</th>
+            </tr>
+            <tr>
+              <td>0-60分</td>
+              <td>不及格</td>
+              <td>0-4000</td>
+              <td>较弱</td>
+            </tr>
+            <tr>
+              <td>60-90分</td>
+              <td>良好</td>
+              <td>4000-7000</td>
+              <td>良好</td>
+            </tr>
+            <tr>
+              <td>90-120分</td>
+              <td>优秀</td>
+              <td>7000-15000</td>
+              <td>优秀</td>
+            </tr>
+          </table>
+          <p class="score-time fr">你离SAT 1600分还需复习<span>5</span>周</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!--  单项结果分析-->
+  <div class="single">
+    <div class="title">
+      <h1>单项结果分析</h1>
+      <p>（友情提示：红色是错误答案，绿色是正确答案，点击你的答案可查看具体题目）</p>
+    </div>
+    <div class="single-cnt">
+<!--      词汇-->
+      <div class="box clearfix">
+        <div class="box-left fl">
+          <div class="single-score clearfix">
+            <p class="fl"><span>10</span>/20分</p>
+            <p class="fl"><i class="fa fa-star"></i>词汇</p>
+          </div>
+          <ul class="single-num">
+            <li class="red"><a href="#">1</a></li>
+            <li class="green"><a href="#">2</a></li>
+            <li class="red"><a href="#">3</a></li>
+            <li class="green"><a href="#">4</a></li>
+            <li class="red"><a href="#">5</a></li>
+            <li class="green"><a href="#">6</a></li>
+            <li class="green"><a href="#">7</a></li>
+            <li class="red"><a href="#">8</a></li>
+            <li class="green"><a href="#">9</a></li>
+            <li class="green"><a href="#">10</a></li>
+          </ul>
+        </div>
+        <div class="box-right fr">
+          <p class="headline"><i class="fa fa-star"></i>你的词汇能力</p>
+          <div class="advice-wrap clearfix">
+            <div class="circle-wrap fr">
+              <p class="circle">0-5分</p>
+              <p>一般</p>
+            </div>
+            <div class="advice-cnt">
+              词汇能力有点弱呀，建议多花些时间丰富词汇量，整理每天所做阅读文章的词汇，多积累扩充词汇量，建议多背雷哥SAT的词汇书。
+            </div>
+          </div>
+        </div>
+      </div>
+<!--      文法-->
+      <div class="box clearfix">
+        <div class="box-left fl">
+          <div class="single-score clearfix">
+            <p class="fl"><span>10</span>/20分</p>
+            <p class="fl"><i class="fa fa-star"></i>语法</p>
+          </div>
+          <ul class="single-num">
+            <li class="red"><a href="#">1</a></li>
+            <li class="green"><a href="#">2</a></li>
+            <li class="red"><a href="#">3</a></li>
+            <li class="green"><a href="#">4</a></li>
+            <li class="red"><a href="#">5</a></li>
+            <li class="green"><a href="#">6</a></li>
+            <li class="green"><a href="#">7</a></li>
+            <li class="red"><a href="#">8</a></li>
+            <li class="green"><a href="#">9</a></li>
+            <li class="green"><a href="#">10</a></li>
+          </ul>
+        </div>
+        <div class="box-right fr">
+          <p class="headline"><i class="fa fa-star"></i>你的文法能力</p>
+          <div class="advice-wrap clearfix">
+            <div class="circle-wrap fr">
+              <p class="circle">0-5分</p>
+              <p>一般</p>
+            </div>
+            <div class="advice-cnt">
+              词汇能力有点弱呀，建议多花些时间丰富词汇量，整理每天所做阅读文章的词汇，多积累扩充词汇量，建议多背雷哥SAT的词汇书。
+            </div>
+          </div>
+        </div>
+      </div>
+<!--      句子翻译-->
+      <div class="box clearfix">
+        <div class="box-left fl">
+          <div class="single-score clearfix">
+            <p class="fl"><span>10</span>/20分</p>
+            <p class="fl"><i class="fa fa-star"></i>句子翻译</p>
+          </div>
+          <ul class="single-num">
+            <li class="red"><a href="#">12</a></li>
+            <li class="green"><a href="#">12</a></li>
+            <li class="red"><a href="#">35</a></li>
+            <li class="green"><a href="#">14</a></li>
+            <li class="red"><a href="#">50</a></li>
+            <li class="green"><a href="#">66</a></li>
+            <li class="green"><a href="#">74</a></li>
+            <li class="red"><a href="#">82</a></li>
+            <li class="green"><a href="#">9</a></li>
+            <li class="green"><a href="#">10</a></li>
+          </ul>
+        </div>
+        <div class="box-right fr">
+          <p class="headline"><i class="fa fa-star"></i>你的句子翻译能力</p>
+          <div class="advice-wrap clearfix">
+            <div class="circle-wrap fr">
+              <p class="circle">0-5分</p>
+              <p>一般</p>
+            </div>
+            <div class="advice-cnt">
+              词汇能力有点弱呀，建议多花些时间丰富词汇量，整理每天所做阅读文章的词汇，多积累扩充词汇量，建议多背雷哥SAT的词汇书。
+            </div>
+          </div>
+        </div>
+      </div>
+<!--      阅读-->
+      <div class="box clearfix">
+        <div class="box-left fl">
+          <div class="single-score clearfix">
+            <p class="fl"><span>10</span>/20分</p>
+            <p class="fl"><i class="fa fa-star"></i>阅读</p>
+          </div>
+          <ul class="single-num">
+            <li class="red"><a href="#">1</a></li>
+            <li class="green"><a href="#">2</a></li>
+            <li class="red"><a href="#">3</a></li>
+            <li class="green"><a href="#">4</a></li>
+            <li class="red"><a href="#">5</a></li>
+            <li class="green"><a href="#">6</a></li>
+            <li class="green"><a href="#">7</a></li>
+            <li class="red"><a href="#">8</a></li>
+            <li class="green"><a href="#">9</a></li>
+            <li class="green"><a href="#">10</a></li>
+          </ul>
+        </div>
+        <div class="box-right fr">
+          <p class="headline"><i class="fa fa-star"></i>你的理解能力</p>
+          <div class="advice-wrap clearfix">
+            <div class="circle-wrap fr">
+              <p class="circle">0-5分</p>
+              <p>一般</p>
+            </div>
+            <div class="advice-cnt">
+              词汇能力有点弱呀，建议多花些时间丰富词汇量，整理每天所做阅读文章的词汇，多积累扩充词汇量，建议多背雷哥SAT的词汇书。
+            </div>
+          </div>
+        </div>
+      </div>
+<!--      数学-->
+      <div class="box clearfix">
+        <div class="box-left fl">
+          <div class="single-score clearfix">
+            <p class="fl"><span>10</span>/20分</p>
+            <p class="fl"><i class="fa fa-star"></i>数学</p>
+          </div>
+          <ul class="single-num">
+            <li class="red"><a href="#">1</a></li>
+            <li class="green"><a href="#">2</a></li>
+            <li class="red"><a href="#">3</a></li>
+            <li class="green"><a href="#">4</a></li>
+            <li class="red"><a href="#">5</a></li>
+            <li class="green"><a href="#">6</a></li>
+            <li class="green"><a href="#">7</a></li>
+            <li class="red"><a href="#">8</a></li>
+            <li class="green"><a href="#">9</a></li>
+            <li class="green"><a href="#">10</a></li>
+          </ul>
+        </div>
+        <div class="box-right fr">
+          <p class="headline"><i class="fa fa-star"></i>你的数学能力</p>
+          <div class="advice-wrap clearfix">
+            <div class="circle-wrap fr">
+              <p class="circle">0-5分</p>
+              <p>一般</p>
+            </div>
+            <div class="advice-cnt">
+              词汇能力有点弱呀，建议多花些时间丰富词汇量，整理每天所做阅读文章的词汇，多积累扩充词汇量，建议多背雷哥SAT的词汇书。
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<!--  整体语言情况分析-->
+  <div class="language">
+    <div class="title">
+      <h1>你的整体语言情况分析</h1>
+    </div>
+    
+  </div>
+<!--  SAT报班建议-->
+  <div class="classes">
+    <div class="title">
+      <h1>SAT报班建议</h1>
+    </div>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+</section>
